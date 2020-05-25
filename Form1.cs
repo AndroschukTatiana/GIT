9b8121c7058bc53d1aa1c9f7087564508eef3c8a
AndroschukTatiana <androschuk_t@inbox.ru>
2020-05-25 13:37:45 +0300

    Добавлены обработчики кнопок

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,15 @@ namespace GIT
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Привет";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
